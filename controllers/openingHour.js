9ff1a29f36d3f8499d9ae9916f40f72c7b790425
Frederik Wouters <woutersf@gmail.com>
2017-12-19 21:26:56 +0100

    entry works

--- controllers/openingHour.js
+++ controllers/openingHour.js
@@ -20,9 +20,19 @@ async function deleteOpeningHour(id) {
 
 async function getOpeningHoursInRange(entity_id, channel_id, from,to) {
     const [rows, fields] =  await dbPool.query('SELECT * FROM opening_hours WHERE entity_id = ? and channel_id = ? AND start_time < ? and end_time > ?  ORDER BY start_time ASC', [entity_id, channel_id, to, from]);
+    var now = new Date();
     rows.forEach(function(item){
         item.start_time_object = new Date(item.start_time * 1000);
         item.end_time_object = new Date(item.end_time * 1000);
+        item.active = false;
+        item.activeClass = 'inactive';
+        if(now.toString('yyyy-MM-dd') == item.start_time_object.toString('yyyy-MM-dd')) {
+            var time = now.getTime();
+            if(time>item.start_time_object.getTime() && time < item.end_time_object.getTime()){
+                item.active = true;
+                item.activeClass = 'active';
+            }
+        }
     });
     return rows;
 }
