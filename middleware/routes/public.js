ccdf268edc1356306902844de4158b46e93e316b
Frederik Wouters <woutersf@gmail.com>
2017-12-20 08:20:21 +0100

    all routes use async await queries

--- middleware/routes/public.js
+++ middleware/routes/public.js
@@ -2,284 +2,231 @@
 const express = require('express');
 const helper = require('./../../controllers/helper');
 const dbPool = require('../../controllers/db');
-const entity = require ('../../controllers/entity');
+const entity = require('../../controllers/entity');
+const channel = require('../../controllers/channel');
 // Setup router for public
 var router = express.Router();              // get an instance of the express Router
 
-router.get('/openinghours/:organisatie_id/:channel_id', function (req, res) {
-  //req.params.organisatie_id
-  var datetimeFrom = req.params.from || helper.formatDateFromJs(new Date());
-  var todate = new Date();
-  todate.setDate(todate.getDate() + 7);
-  var datetimeTo = req.params.to || helper.formatDateFromJs(todate);
+router.get('/openinghours/:entity_id/:channel_id', async function (req, res) {
+    //req.params.entity_id
+    var datetimeFrom = req.params.from || helper.formatDateFromJs(new Date());
+    var todate = new Date();
+    todate.setDate(todate.getDate() + 7);
+    var datetimeTo = req.params.to || helper.formatDateFromJs(todate);
+    const [results, fields] = await
+    dbPool.query('SELECT * ' +
+        'FROM opening_hours ' +
+        'WHERE entity_id = ? ' +
+        '   AND channel_id = ?' +
+        '   AND day >= ?' +
+        '   AND day <= ?' +
+        ' ORDER BY day ASC, start_time ASC',
+        [
+            req.params.entity_id,
+            req.params.channel_id,
+            datetimeFrom,
+            datetimeTo
+        ]);
+    var $days = helper.transformQueryResultsToDaysOutput(results);
+    //console.log($days);
+    res.json({days: $days});
+}
+)
+;
 
-  // console.log(datetimeTo);
-  // console.log(datetimeFrom);
-  // console.log(req.params.organisatie_id);
-  // console.log(req.params.channel_id);
+router.get('/openinghours/:entity_id', async function (req, res) {
+    //req.params.entity_id
+    var datetimeFrom = req.params.from || helper.formatDateFromJs(new Date());
+    var todate = new Date();
+    todate.setDate(todate.getDate() + 7);
+    var datetimeTo = req.params.to || helper.formatDateFromJs(todate);
+    const [results, fields] = await
+    dbPool.query('SELECT * ' +
+        'FROM opening_hours ' +
+        'WHERE entity_id = ? ' +
+        '   AND day >= ?' +
+        '   AND day <= ?' +
+        ' ORDER BY day ASC, start_time ASC',
+        [
+            req.params.entity_id,
+            datetimeFrom,
+            datetimeTo
+        ]);
+    var $channels = helper.transformQueryResultsToChannelDaysOutput(results);
+    //console.log($channels);
+    res.json({channels: $channels});
+}
+)
+;
+router.get('/openinghours', async function (req, res) {
+    var datetimeFrom = req.params.from || helper.formatDateFromJs(new Date());
+    var todate = new Date();
+    todate.setDate(todate.getDate() + 7);
+    var datetimeTo = req.params.to || helper.formatDateFromJs(todate);
 
-  dbPool.query('SELECT * ' +
-    'FROM opening_hours ' +
-    'WHERE organisation_id = ? ' +
-    '   AND channel_id = ?' +
-    '   AND day >= ?' +
-    '   AND day <= ?' +
-    ' ORDER BY day ASC, start_time ASC',
-    [
-      req.params.organisatie_id,
-      req.params.channel_id,
-      datetimeFrom,
-      datetimeTo
-    ],
-    function (error, results, fields) {
-      //console.log(typeof error);
-      if (!error) {
-        var $days = helper.transformQueryResultsToDaysOutput(results);
-        //console.log($days);
-        res.json({days: $days});
+    const [results, fields] = await
+    dbPool.query('SELECT * ' +
+        'FROM opening_hours ' +
+        '   WHERE day >= ?' +
+        '   AND day <= ?' +
+        'ORDER BY day ASC, start_time ASC',
+        [
+            datetimeFrom,
+            datetimeTo
+        ]);
+    var $channels = helper.transformQueryResultsToOrgChannelDaysOutput(results);
+    res.json({entities: $channels});
 
-      }
-      // error will be an Error if one occurred during the query
-      // results will contain the results of the query
-      // fields will contain information about the returned results fields (if any)
-    });
-});
-router.get('/openinghours/:organisatie_id', function (req, res) {
-  //req.params.organisatie_id
-  var datetimeFrom = req.params.from || helper.formatDateFromJs(new Date());
-  var todate = new Date();
-  todate.setDate(todate.getDate() + 7);
-  var datetimeTo = req.params.to || helper.formatDateFromJs(todate);
-
-  // console.log(datetimeTo);
-  // console.log(datetimeFrom);
-  // console.log(req.params.organisatie_id);
-  // console.log(req.params.channel_id);
-
-  dbPool.query('SELECT * ' +
-    'FROM opening_hours ' +
-    'WHERE organisation_id = ? ' +
-    '   AND day >= ?' +
-    '   AND day <= ?' +
-    ' ORDER BY day ASC, start_time ASC',
-    [
-      req.params.organisatie_id,
-      datetimeFrom,
-      datetimeTo
-    ],
-    function (error, results, fields) {
-      //console.log(typeof error);
-      if (!error) {
-        var $channels = helper.transformQueryResultsToChannelDaysOutput(results);
-        //console.log($channels);
-        res.json({channels: $channels});
-
-      }
-      // error will be an Error if one occurred during the query
-      // results will contain the results of the query
-      // fields will contain information about the returned results fields (if any)
-    });
-});
-router.get('/openinghours', function (req, res) {
-  var datetimeFrom = req.params.from || helper.formatDateFromJs(new Date());
-  var todate = new Date();
-  todate.setDate(todate.getDate() + 7);
-  var datetimeTo = req.params.to || helper.formatDateFromJs(todate);
-
-  // console.log(datetimeTo);
-  // console.log(datetimeFrom);
-  //console.log(req.params.organisatie_id);
-  //console.log(req.params.channel_id);
-
-  dbPool.query('SELECT * ' +
-    'FROM opening_hours ' +
-    '   WHERE day >= ?' +
-    '   AND day <= ?' +
-    'ORDER BY day ASC, start_time ASC',
-    [
-      datetimeFrom,
-      datetimeTo
-    ],
-    function (error, results, fields) {
-      //console.log(typeof error);
-      if (!error) {
-        var $channels = helper.transformQueryResultsToOrgChannelDaysOutput(results);
-        //console.log($channels);
-        res.json({entities: $channels});
-
-      }
-      // error will be an Error if one occurred during the query
-      // results will contain the results of the query
-      // fields will contain information about the returned results fields (if any)
-    });
-});
+}
+)
+;
 
 //////////////////// OPEN ROUTES
 //                  GEOPEND
-router.get('/open/:organisatie_id/:channel_id', function (req, res) {
-  //req.params.organisatie_id
-  //req.params.channel_id
-  //optionele url parameter: timestamp (toon status on timestamp).
-  var datetime = req.params.timestamp || Math.floor(Date.now() / 1000);
-  var huidigeDag = helper.formatDateFromUnix(datetime);
-  // console.log(datetime);
-  // console.log(huidigeDag);
-  // console.log(req.params.organisatie_id);
-  // console.log(req.params.channel_id);
-  dbPool.query('SELECT count(id) as existing FROM opening_hours ' +
-    'WHERE entity_id = ?  AND channel_id = ?  AND day = ?  AND start_time <= ?  AND end_time >= ?',
-    [
-      req.params.organisatie_id,
-      req.params.channel_id,
-      huidigeDag,
-      datetime,
-      datetime
-    ],
-    function (error, results) {
-      var $geopend = false;
-      if (error) {
-        //console.log(error);
-      } else {
-        //console.log('FOUND:');
-        //console.log(results[0].existing);
-        if (results[0].existing == 0) {
-          //do nothing
-          $geopend = false;
-        }
-        if (results[0].existing == 1 || results[0].existing > 1) {
-          $geopend = true;
-        }
-      }
-      res.json({geopend: $geopend});
-      // error will be an Error if one occurred during the query
-      // results will contain the results of the query
-      // fields will contain information about the returned results fields (if any)
+router.get('/open/:entity_id/:channel_id', async function (req, res) {
+    //req.params.entity_id
+    //req.params.channel_id
+    //optionele url parameter: timestamp (toon status on timestamp).
+    var datetime = req.params.timestamp || Math.floor(Date.now() / 1000);
+    var huidigeDag = helper.formatDateFromUnix(datetime);
+    // console.log(datetime);
+    // console.log(huidigeDag);
+    // console.log(req.params.entity_id);
+    // console.log(req.params.channel_id);
+    const [results,fields] = await dbPool.query('SELECT count(id) as existing FROM opening_hours ' +
+        'WHERE entity_id = ?  AND channel_id = ?  AND day = ?  AND start_time <= ?  AND end_time >= ?',
+        [
+            req.params.entity_id,
+            req.params.channel_id,
+            huidigeDag,
+            datetime,
+            datetime
+        ]);
+    var $geopend = false;
+    if (results[0].existing == 0) {
+        $geopend = false;
+    }
+    if (results[0].existing == 1 || results[0].existing > 1) {
+        $geopend = true;
+    }
+    res.json({geopend: $geopend});
     });
-});
 
-router.get('/open/:organisatie_id', function (req, res) {
-  //req.params.organisatie_id
-  //req.params.channel_id
-  //optionele url parameter: timestamp (toon status on timestamp).
+router.get('/open/:entity_id', async function (req, res) {
+    //req.params.entity_id
+    //req.params.channel_id
+    //optionele url parameter: timestamp (toon status on timestamp).
 
-  //req.params.organisatie_id
-  //req.params.channel_id
-  //optionele url parameter: timestamp (toon status on timestamp).
-  var datetime = req.params.timestamp || Math.floor(Date.now() / 1000);
-  var huidigeDag = helper.formatDateFromUnix(datetime);
-  // console.log(datetime);
-  // console.log(huidigeDag);
-  // console.log(req.params.organisatie_id);
-  // console.log(req.params.channel_id);
-  dbPool.query('SELECT  channel_id, count(id) as existing FROM opening_hours ' +
-    'WHERE entity_id = ? AND day = ?  AND start_time <= ?  AND end_time >= ? GROUP BY  channel_id',
-    [
-      req.params.organisatie_id,
-      huidigeDag,
-      datetime,
-      datetime
-    ],
-    function (error, results) {
-      var $geopend = false;
-      if (error) {
-        console.log(error);
-      } else {
-        // console.log('FOUND:');
-        // console.log(results);
+    //req.params.entity_id
+    //req.params.channel_id
+    //optionele url parameter: timestamp (toon status on timestamp).
+    var datetime = req.params.timestamp || Math.floor(Date.now() / 1000);
+    var huidigeDag = helper.formatDateFromUnix(datetime);
+    const [results,fields] = await dbPool.query('SELECT  channel_id, count(id) as existing FROM opening_hours ' +
+        'WHERE entity_id = ? AND day = ?  AND start_time <= ?  AND end_time >= ? GROUP BY  channel_id',
+        [
+            req.params.entity_id,
+            huidigeDag,
+            datetime,
+            datetime
+        ]);
+
         var $outputArray = {};
 
         results.forEach(function (item) {
 
-          if (typeof $outputArray[item.channel_id] == 'undefined') {
-            $outputArray[item.channel_id] = {
-              channel_id: item.channel_id,
-              geopend: (item.existing > 0) ? true : false
-            };
-          }
+            if (typeof $outputArray[item.channel_id] == 'undefined') {
+                $outputArray[item.channel_id] = {
+                    channel_id: item.channel_id,
+                    geopend: (item.existing > 0) ? true : false
+                };
+            }
         });
-      }
-      res.json({channels: $outputArray});
-      // error will be an Error if one occurred during the query
-      // results will contain the results of the query
-      // fields will contain information about the returned results fields (if any)
-    });
+
+        res.json({channels: $outputArray});
 });
 
 //voor intern gebruik _ overzichtpagina
-router.get('/open', function (req, res) {
-  //req.params.organisatie_id
-  //req.params.channel_id
-  //optionele url parameter: timestamp (toon status on timestamp).
-  //SELECT entity_id , channel_id, count(id) as existing  FROM opening_hours WHERE day = '2017-12-10'  AND start_time <= 1512915217 AND end_time >= 1512915217 GROUP BY entity_id, channel_id
+router.get('/open', async function (req, res) {
+    //req.params.entity_id
+    //req.params.channel_id
+    //optionele url parameter: timestamp (toon status on timestamp).
+    //SELECT entity_id , channel_id, count(id) as existing  FROM opening_hours WHERE day = '2017-12-10'  AND start_time <= 1512915217 AND end_time >= 1512915217 GROUP BY entity_id, channel_id
 
-  //req.params.organisatie_id
-  //req.params.channel_id
-  //optionele url parameter: timestamp (toon status on timestamp).
-  var datetime = req.params.timestamp || Math.floor(Date.now() / 1000);
-  var huidigeDag = helper.formatDateFromUnix(datetime);
-  // console.log(datetime);
-  // console.log(huidigeDag);
-  // console.log(req.params.organisatie_id);
-  // console.log(req.params.channel_id);
-  dbPool.query('SELECT entity_id , channel_id, count(id) as existing FROM opening_hours ' +
-    'WHERE day = ?  AND start_time <= ?  AND end_time >= ? GROUP BY entity_id, channel_id',
-    [
-      huidigeDag,
-      datetime,
-      datetime
-    ],
-    function (error, results) {
-      var $geopend = false;
-      if (error) {
-        console.log(error);
-      } else {
-        // console.log('FOUND:');
-        // console.log(results);
-        var $outputArray = {};
+    //req.params.entity_id
+    //req.params.channel_id
+    //optionele url parameter: timestamp (toon status on timestamp).
+    var datetime = req.params.timestamp || Math.floor(Date.now() / 1000);
+    var huidigeDag = helper.formatDateFromUnix(datetime);
+    // console.log(datetime);
+    // console.log(huidigeDag);
+    // console.log(req.params.entity_id);
+    // console.log(req.params.channel_id);
+    const [results, fields] = await
+    dbPool.query('SELECT entity_id , channel_id, count(id) as existing FROM opening_hours ' +
+        'WHERE day = ?  AND start_time <= ?  AND end_time >= ? GROUP BY entity_id, channel_id',
+        [
+            huidigeDag,
+            datetime,
+            datetime
+        ]);
+    var $outputArray = {};
 
-        results.forEach(function (item) {
-          if (typeof $outputArray[item.entity_id] == 'undefined') {
+    results.forEach(function (item) {
+        if (typeof $outputArray[item.entity_id] == 'undefined') {
             $outputArray[item.entity_id] = {entity_id: item.entity_id, channels: {}};
-          }
-          if (typeof $outputArray[item.entity_id].channels[item.channel_id] == 'undefined') {
+        }
+        if (typeof $outputArray[item.entity_id].channels[item.channel_id] == 'undefined') {
             $outputArray[item.entity_id].channels[item.channel_id] = {
-              channel_id: item.channel_id,
-              geopend: (item.existing > 0) ? true : false
+                channel_id: item.channel_id,
+                geopend: (item.existing > 0) ? true : false
             };
-          }
-        });
-      }
-      res.json({entities: $outputArray});
-      // error will be an Error if one occurred during the query
-      // results will contain the results of the query
-      // fields will contain information about the returned results fields (if any)
+        }
     });
+
+    res.json({entities: $outputArray});
 });
 
 //voor intern gebruik _ overzichtpagina
-router.get('/entities', function (req, res) {
-  res.json({entities: global.entities});
-});
+router.get('/entities', async function (req, res) {
+    try {
+    var entities = await
+    entity.loadEntities();
+    res.json({entities: entities});
+    } catch (err) {
+        console.error(err)
+    }
+}
+)
+;
 
-router.get('/entities/:entity_id', function (req, res) {
-  var $entity = entity.loadEntity(req.params.entity_id);
-  console.log($entity);
-  res.json({entity: $entity});
-
+router.get('/entities/:entity_id', async function (req, res) {
+    try {
+        var $entity = entity.loadEntity(req.params.entity_id);
+        console.log($entity);
+        res.json({entity: $entity});
+    } catch (err) {
+        console.error(err)
+    }
 });
 
 //voor intern gebruik _ overzichtpagina
-router.get('/channels', function (req, res) {
-
-  res.json({channels: global.channels});
-
-});
+router.get('/channels', async function (req, res) {
+    try {
+        var entities = await channel.loadChannels();
+        res.json({channels: entities});
+    } catch (err) {
+        console.error(err)
+    }
+}
+)
+;
 
 router.get('/', function (req, res) {
-  res.json({
-    message: 'OPENINGSUREN!',
-    routes: router.stack
-  });
+    res.json({
+        message: 'OPENINGSUREN!',
+        routes: router.stack
+    });
 });
 
 // Export the router
